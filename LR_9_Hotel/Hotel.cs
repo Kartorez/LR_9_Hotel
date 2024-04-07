e08604ec025265d7c548855c8edc2fd6f502bcfa
Kartorez <Bmatvienko05@gmail.com>
2024-04-07 21:41:58 +0300

    Добавьте файлы проекта.

---
+++ LR_9_Hotel/Hotel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_9_Hotel
+{
+    internal class Hotel
+    {
+        public string Name;
+        public string Adrress;
+        public double Stars;
+        public double Rate;
+
+
+        public Hotel(string name, string adrress, double stars, double rate)
+        {
+            Name = name;
+            Adrress = adrress;
+            Stars = stars;
+            Rate = rate;
+        }
+    }
+    class HotelManager { 
+
+        public List<Hotel> hotels;
+
+        public HotelManager()
+        {
+            hotels = new List<Hotel>(); 
+        }
+
+        public void AddHotel(Hotel hotel)
+        {
+            hotels.Add(hotel);  
+        }
+        public void ShowHotel()
+        {
+            Console.WriteLine("Список готелів");
+            foreach (var hotel in hotels)
+            {
+                Console.WriteLine($"Назва: {hotel.Name} Адреса: {hotel.Adrress} Рейтинг: {hotel.Rate} Зірки: {hotel.Stars}");
+            }
+        }
+            public void RemoveHotel(string name)
+            {
+                foreach (var hotel in hotels)
+                {
+                    if (hotel.Name == name)
+                    {
+                        hotels.Remove(hotel);
+                    break;
+                    }
+                }
+                
+            }
+
+            public Hotel GetHotelHighRate()
+            {
+                Hotel HighestRatedHotel = null;
+                double MaxRate = 0;
+
+                foreach (var hotel in hotels)
+                {
+                    if (hotel.Rate > MaxRate)
+                    {
+                        HighestRatedHotel = hotel;
+                        MaxRate = hotel.Rate;
+                    }
+                }
+
+                return HighestRatedHotel;
+            }
+
+            public List<Hotel> GetHotelsWithRating(double MinRate)
+            {
+                List<Hotel> HighRatedHotels = new List<Hotel>();
+
+                foreach (var hotel in hotels)
+                {
+                    if (hotel.Rate >= MinRate)
+                    {
+                        HighRatedHotels.Add(hotel);
+                    }
+                }
+
+                return HighRatedHotels;
+            }
+            
+        }
+     }
+
+   
---
+++ LR_9_Hotel/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_9_Hotel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            HotelManager Manager = new HotelManager();
+
+            Manager.AddHotel(new Hotel("Hotel A", "Address A", 4, 8.5));
+            Manager.AddHotel(new Hotel("Hotel B", "Address B", 3, 7.2));
+            Manager.AddHotel(new Hotel("Hotel C", "Address C", 5, 9.3));
+            Manager.ShowHotel();
+            Manager.RemoveHotel("Hotel B");
+            Manager.ShowHotel();
+
+            Hotel HighestRatedHotel = Manager.GetHotelHighRate();
+            Console.WriteLine($"Готелі з найбільшим рейтенгом: {HighestRatedHotel.Name}, Рейтинг: {HighestRatedHotel.Rate}");
+
+    
+            List<Hotel> HighRatedHotels = Manager.GetHotelsWithRating(9.0);
+            Console.WriteLine("Готелі з рейтенгом 9.0 і вище:");
+            foreach (var hotel in HighRatedHotels)
+            {
+                Console.WriteLine($"Назва: {hotel.Name} Рейтенг: {hotel.Rate}");
+            }
+            Console.ReadLine();
+
+            /*Список готелів
+            Назва: Hotel A Адреса: Address A Рейтинг: 8,5 Зірки: 4
+            Назва: Hotel B Адреса: Address B Рейтинг: 7,2 Зірки: 3
+            Назва: Hotel C Адреса: Address C Рейтинг: 9,3 Зірки: 5
+            Список готелів
+            Назва: Hotel A Адреса: Address A Рейтинг: 8,5 Зірки: 4
+            Назва: Hotel C Адреса: Address C Рейтинг: 9,3 Зірки: 5
+            Готелі з найбільшим рейтенгом: Hotel C, Рейтинг: 9,3
+            Готелі з рейтенгом 9.0 і вище:
+            Назва: Hotel C Рейтенг: 9,3*/
+        }
+    }
+}
